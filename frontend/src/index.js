21df6f2bda2ea829849dfa9ca5cd584fc0ed6ccb
Shivas Jayaram <shivasj@gmail.com>
2021-11-22 18:18:07 -0500

    Frontend template files

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './template1/app/App';
+//import App from './template2/app/App';
+//import App from './template3/app/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
