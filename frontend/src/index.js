583c8760a16a83b7e9917cc629046ee7b935421e
Shivas Jayaram <shivasj@gmail.com>
2021-11-28 15:02:32 -0500

    Made main index.js switch templates based on query string

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './template1/app/App';
-//import App from './template2/app/App';
-//import App from './template3/app/App';
+import App1 from './template1/app/App';
+import App2 from './template2/app/App';
+//import App3 from './template3/app/App';
+
+console.log(window.location)
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+console.log(params)
+let template = '1';
+if (params["template"]) {
+  template = params["template"]
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {template == '1' && <App1 />}
+    {template == '2' && <App2 />}
   </React.StrictMode>,
   document.getElementById('root')
 );
